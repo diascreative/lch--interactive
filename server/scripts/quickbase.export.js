e4daf6505842ad4a78c8d57c66188380241ecd14
pire <rob.cmm@gmail.com>
2017-06-05 10:42:08 +0200

    feat: update QB tables after emig import script has run

--- server/scripts/quickbase.export.js
+++ server/scripts/quickbase.export.js
@@ -24,7 +24,7 @@ export function scheduleJobs() {
   console.log('schedules quickbase export jobs');
   // exportYesterdayToQuickBase();
   // exportYesterdayToQuickBase();
-  schedule.scheduleJob('0 7 * * *', exportYesterdayToQuickBase);
+  schedule.scheduleJob('0 9 * * *', exportYesterdayToQuickBase);
 }
 
 /**
